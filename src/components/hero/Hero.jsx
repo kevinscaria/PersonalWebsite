7f8cbb7254792f767445115f1600a2022df6b844
Kevin Scaria <scariakevin1@gmail.com>
2023-10-12 21:54:44 -0700

    Changed to Hero page

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -113,7 +113,7 @@ const Hero = () => {
               // src={mine}
               alt="mine"
             />
-            <div className=" absolute bottom-10 md:bottom-3 right-8 md:right-2">
+            {/* <div className=" absolute bottom-10 md:bottom-3 right-8 md:right-2">
               <div data-aos="zoom-in" data-aos-duration="1000"  className=" relative cursor-pointer">
                 <img
                   className=" w-[135px] md:w-[90px] circle-text"
@@ -124,7 +124,7 @@ const Hero = () => {
                   className=" text-black absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]"
                 />
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -66,7 +66,7 @@ const NavBar = ({ toggleDarkMode, darkMode }) => {
             to="/"
             className="text-3xl font-bold sm:text-3xl"
           >
-            John Doe.
+            Kevin Joseph Scaria
           </Link>
         </div>
         <div
